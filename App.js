b7209735651034845f1371ef6ce66e3ed660c093
abhishek <abhishektupsundar1@gmail.com>
2023-12-11 10:21:24 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,33 @@
+const ratings=document.querySelectorAll(".rating")
+const ratingsContainer=document.querySelector(".ratings-container")
+const sendBtn=document.querySelector("#send")
+const panel=document.querySelector("#panel")
+
+
+let selectedRating="Satisfied"
+
+ratingsContainer.addEventListener("click",(e)=>
+{
+    if(e.target.parentNode.classList.contains("rating")){
+        // console.log(e.target.parentNode.classList.contains("rating"));
+
+        removeActive()
+        e.target.parentNode.classList.add("active")
+        selectedRating=e.target.nextElementSibling.innerHTML;
+    }
+})
+
+sendBtn.addEventListener("click",()=>
+{
+    panel.innerHTML=`
+    <p class="heart"> ❤ </p>
+    <strong>Thank You!</strong>
+    <br>
+    <Strong>Feedback:${selectedRating}</Strong>`
+})
+
+function removeActive(){
+    for(let i=0;i<ratings.length;i++){
+        ratings[i].classList.remove("active")
+    }
+}
